cb5d7705e6573d4a6ff63e937126024c1d3e40da
Willy Kiptoo <kiptoowilly1@gmail.com>
2025-11-04 00:00:45 +0300

    Add basic arithmetic operations script

---
+++ alx_be_python/python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
+
+git add basic_operations.py
+git commit -m "Add basic arithmetic operations script"
+git push -u origin main
